ed136e38f911b7461b455d234c6c39d25b0d987d
jjwm10625 <jjwm10625@sookmyung.ac.kr>
2025-10-14 17:48:14 +0900

    [chore] 텍스트필드 속성 추가

--- week_01/week_01/LoginViewController.swift
+++ week_01/week_01/LoginViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 final class LoginViewController: UIViewController {
     
+    // MARK: - UI Components
+    
     private let titleLabel: UILabel = {
         let label = UILabel(frame: CGRect(x: 69, y: 161, width: 236, height: 44))
         label.text = "동네라서 가능한 모든것\n당근에서 가까운 이웃과 함께해요."
@@ -22,23 +24,31 @@ final class LoginViewController: UIViewController {
     private let idTextField: UITextField = {
         let textField = UITextField(frame: CGRect(x: 20, y: 276, width: 335, height: 52))
         textField.placeholder = "아이디를 입력해주세요"
-        textField.font = UIFont(name: "Pretendard-SemiBold", size: 14)
+        textField.font = UIFont(name: "Pretendard-Regular", size: 16)
         textField.backgroundColor = .grey200
-        textField.layer.cornerRadius = 3
+        textField.layer.cornerRadius = 6
+        textField.clearButtonMode = .whileEditing   // 입력 중 X버튼 표시하는 속성입니다.
+        textField.setPadding(10) // 텍스트필드에 패딩 넣을 일이 많아서, 편하게 쓰려고 Extension으로 따로 빼뒀어요!
+        
         return textField
     }()
     
     private let passwordTextField: UITextField = {
         let textField = UITextField(frame: CGRect(x: 20, y: 335, width: 335, height: 52))
         textField.placeholder = "비밀번호를 입력해주세요"
-        textField.font = UIFont(name: "Pretendard-SemiBold", size: 14)
+        textField.font = UIFont(name: "Pretendard-Regular", size: 16)
         textField.backgroundColor = .grey200
-        textField.layer.cornerRadius = 3
+        textField.layer.cornerRadius = 6
+        textField.clearButtonMode = .whileEditing
+        textField.isSecureTextEntry = true
+        textField.setPadding(10)
+        
         return textField
     }()
     
+    
     private lazy var loginButton: UIButton = {
-        let button = UIButton(frame: CGRect(x: 21, y: 422, width: 332, height: 58))
+        let button = UIButton(frame: CGRect(x: 21, y: 422, width: 332, height: 52))
         button.backgroundColor = .primaryOrange
         button.setTitle("로그인하기", for: .normal)
         button.setTitleColor(.white, for: .normal)
@@ -48,36 +58,46 @@ final class LoginViewController: UIViewController {
         return button
     }()
     
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.view.backgroundColor = .white
+        view.backgroundColor = .white
         setLayout()
     }
     
+    // MARK: - Setup Methods
+    
     private func setLayout() {
         [titleLabel, idTextField, passwordTextField, loginButton].forEach {
-            self.view.addSubview($0)
+            view.addSubview($0)
         }
     }
+    
+    // MARK: - Navigation
     
     private func presentToWelcomeVC() {
         let welcomeViewController = WelcomeViewController()
         welcomeViewController.modalPresentationStyle = .formSheet
         welcomeViewController.id = idTextField.text
-        self.present(welcomeViewController, animated: true)
+        present(welcomeViewController, animated: true)
     }
-
+    
     private func pushToWelcomeVC() {
         let welcomeViewController = WelcomeViewController()
         welcomeViewController.id = idTextField.text
-        self.navigationController?.pushViewController(welcomeViewController, animated: true)
+        navigationController?.pushViewController(welcomeViewController, animated: true)
     }
+    
+    // MARK: - Actions
     
     @objc
     private func loginButtonDidTap() {
-//        presentToWelcomeVC()
+        //presentToWelcomeVC()
         pushToWelcomeVC()
     }
 }
 
+#Preview {
+    LoginViewController()
+}
