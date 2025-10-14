280a2ff07e07c335861dac7c114cc59c9a46a4e7
jjwm10625 <jjwm10625@sookmyung.ac.kr>
2025-10-14 18:56:11 +0900

    [chore] WelcomeVC 수정

--- week_01/week_01/WelcomeViewController.swift
+++ week_01/week_01/WelcomeViewController.swift
@@ -9,17 +9,21 @@ import UIKit
 
 final class WelcomeViewController: UIViewController {
     
+    // MARK: - Properties
+    
     var id: String?
     
+    // MARK: - UI Components
+    
     private let logoImageView: UIImageView = {
-        let imageView = UIImageView(frame: CGRect(x: 112, y: 87, width: 150, height: 150))
+        let imageView = UIImageView(frame: CGRect(x: 112, y: 90, width: 150, height: 150))
         imageView.image = UIImage(named: "character_profile")
         return imageView
     }()
     
     private let welcomeLabel: UILabel = {
         let label = UILabel(frame: CGRect(x: 140, y: 295, width: 95, height: 60))
-        label.font = UIFont(name: "Pretendard-ExtraBold", size: 25)
+        label.font = UIFont(name: "Pretendard-Bold", size: 25)
         label.textAlignment = .center
         label.numberOfLines = 2
         return label
@@ -39,7 +43,7 @@ final class WelcomeViewController: UIViewController {
         let button = UIButton(frame: CGRect(x: 20, y: 498, width: 335, height: 58))
         button.backgroundColor = .grey200
         button.setTitle("다시 로그인", for: .normal)
-        button.setTitleColor(UIColor(red: 172/255, green: 176/255, blue: 185/255, alpha: 1), for: .normal)
+        button.setTitleColor(.grey400, for: .normal)
         button.titleLabel?.font = UIFont(name: "Pretendard-Bold", size: 18)
         button.layer.cornerRadius = 6
         return button
@@ -49,18 +53,15 @@ final class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        navigationItem.hidesBackButton = true
+        navigationItem.hidesBackButton = true //상단에 표시되는 기본 네비게이션 바의 뒤로가기 버튼을 숨기는 속성입니다!
         view.backgroundColor = .white
-        
         setLayout()
         bindID()
-        
         backToLoginButton.addTarget(self, action: #selector(backToLoginButtonDidTap), for: .touchUpInside)
         goHomeButton.addTarget(self, action: #selector(goHomeButtonDidTap), for: .touchUpInside)
     }
     
-    // MARK: - Private Methods
+    // MARK: - Setup Methods
     
     private func setLayout() {
         [logoImageView, welcomeLabel, goHomeButton, backToLoginButton].forEach {
@@ -68,11 +69,13 @@ final class WelcomeViewController: UIViewController {
         }
     }
     
+    // MARK: - Private Methods
+    
     private func bindID() {
         if let idText = id, !idText.isEmpty {
             welcomeLabel.text = "\(idText)님 \n반가워요!"
         } else {
-            welcomeLabel.text = "뿅 ㅋㅋ"
+            welcomeLabel.text = "뿅ㅋㅋ"
         }
     }
     
@@ -88,6 +91,9 @@ final class WelcomeViewController: UIViewController {
     }
     
     @objc
-    private func goHomeButtonDidTap() {
-    }
+    private func goHomeButtonDidTap() { }
+}
+
+#Preview {
+    WelcomeViewController()
 }
