930578aefb32f05e833afc8b29879559ef1a7708
jjwm10625 <jjwm10625@sookmyung.ac.kr>
2025-11-29 17:26:13 +0900

    [feat] 7주차 실습과제 구현

---
+++ week_07/week_07/week_07App.swift
@@ -0,0 +1,17 @@
+//
+//  week_07App.swift
+//  week_07
+//
+//  Created by 조영서 on 11/29/25.
+//
+
+import SwiftUI
+
+@main
+struct week_07App: App {
+    var body: some Scene {
+        WindowGroup {
+            DestinationView()
+        }
+    }
+}
